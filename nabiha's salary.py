56d9164e15ca3910954bde853b50789abf6a6124
souleimanelali <souleiman.elali@gmail.com>
2025-02-20 23:28:27 +0200

    Add nabiha's salary file by creating Monthly Budget Allocator for Nabiha

---
+++ nabiha's salary.py
@@ -0,0 +1,13 @@
+import random
+
+# Monthly Budget Allocator for Nabiha
+def main():
+    try:
+        month = input("Enter the month you are storing the salary for: ")
+        salary = float(input("Enter your salary for the month: "))
+        
+        
+        
+
+if __name__ == "__main__":
+    main()
