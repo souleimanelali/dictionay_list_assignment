e2f5144b6ebc736263c5c4722176043ef7ec0561
souleimanelali <souleiman.elali@gmail.com>
2025-02-20 23:43:35 +0200

    update nabiha's salary where Geting custom percentage allocations

--- nabiha's salary.py
+++ nabiha's salary.py
@@ -6,7 +6,10 @@ def main():
         month = input("Enter the month you are storing the salary for: ")
         salary = float(input("Enter your salary for the month: "))
         
-        
+        # Get custom percentage allocations
+        savings_pct = float(input("Enter the percentage for Savings: ")) / 100
+        rent_pct = float(input("Enter the percentage for Rent: ")) / 100
+        electricity_pct = float(input("Enter the percentage for Electricity: ")) / 100
         
 
 if __name__ == "__main__":
